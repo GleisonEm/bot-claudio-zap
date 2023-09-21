b5ca940cc7e845b183d43ead40156ef517b3b52f
Gleison Emanuel <gleison.spfc1208@hotmail.com>
2023-09-21 18:24:01 -0300

    update

---
+++ clientRepository.js
@@ -0,0 +1,161 @@
+const { writeFile, readFile,open,close, unlink } = require('fs');
+const fs = require('fs/promises');
+const type = '.json';
+
+const createFile = async (fileName, argument)=>{
+    open(`data/${fileName}${type}`, 'wx', (err, fd) => {
+        if (err) {
+          console.log(err.message);
+        }
+
+        let defaultFile = {
+          title: "",
+          users: []
+        }
+        if(argument.length > 0){
+          defaultFile.title = argument
+        }
+        writeFile(`data/${fileName}${type}`, JSON.stringify(defaultFile, null, 2), (err) => {
+          if (err) {
+            console.log('Failed to write updated data to file');
+            return;
+          }
+          console.log('Updated file successfully');
+        });
+        close(fd, err => {
+          if (err) {
+            console.log(err.message);
+          }
+        });
+      });
+
+}
+
+const deleteFile = async (fileName)=>{
+    unlink(`data/${fileName}${type}`, (err) => {
+        if (err) {
+          console.log(err.message);
+          return;
+        }
+
+        console.log("Sucesso ao deletar")
+      });
+
+}
+
+const addInList = async (fileName, users)=>{
+
+    readFile(`data/${fileName}${type}`, (error, data) => {
+        if (error) {
+          console.log(error);
+          return;
+        }
+        let parsedData = [];
+        console.log(data)
+        if(data.length > 0){
+            parsedData  = JSON.parse(data);
+        }
+
+        parsedData.users.push(...users.map(u => {return {id: u.id.user,serialized: u.id._serialized}}))
+
+        writeFile(`data/${fileName}${type}`, JSON.stringify(parsedData, null, 2), (err) => {
+          if (err) {
+            console.log('Failed to write updated data to file');
+            return;
+          }
+          console.log('Updated file successfully');
+        });
+      });
+}
+
+const getList = async (fileName)=>{
+     const listUsers = await fs.readFile(`data/${fileName}${type}`);
+        let parsedData = {};
+
+        if(listUsers.length > 0){
+            parsedData  = JSON.parse(listUsers);
+        }
+        console.log(parsedData)
+        return parsedData
+}
+
+const getRulesList = async (sender)=>{
+  let parsedData = [];
+
+  try {
+
+    const listUsers = await fs.readFile(`data/${sender}${type}`);
+    parsedData  = JSON.parse(listUsers);
+  } catch (error) {
+
+    writeFile(`data/${sender}${type}`, JSON.stringify([], null, 2), (err) => {
+      if (err) {
+        console.log('erro recuperar regras');
+        return;
+      }
+      console.log('regras retornadas com sucesso');
+    });
+    close(fd, err => {
+      if (err) {
+        console.log(err.message);
+      }
+    });
+
+  }finally{
+    return parsedData;
+  }
+
+}
+
+const addRule = async (sender, rule)=>{
+
+  readFile(`data/${sender}${type}`, (error, data) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      let parsedData = [];
+      console.log(data)
+      if(data.length > 0){
+          parsedData  = JSON.parse(data);
+      }
+
+      parsedData.push(rule)
+
+      writeFile(`data/${sender}${type}`, JSON.stringify(parsedData, null, 2), (err) => {
+        if (err) {
+          console.log('Failed to write updated data to file');
+          return;
+        }
+        console.log('Updated file successfully');
+      });
+    });
+}
+
+const editRule = async (sender, rule)=>{
+
+  readFile(`data/${sender}${type}`, (error, data) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      let parsedData = [];
+      console.log(data)
+      if(data.length > 0){
+          parsedData  = JSON.parse(data);
+      }
+      let ruleData = {number:rule.split(" ")[0], data: rule.substring(rule.indexOf(" ")).trim()}
+      parsedData[parseInt(ruleData.number-1)] = ruleData.data
+
+      writeFile(`data/${sender}${type}`, JSON.stringify(parsedData, null, 2), (err) => {
+        if (err) {
+          console.log('Failed to write updated data to file');
+          return;
+        }
+        console.log('Updated file successfully');
+      });
+    });
+}
+
+module.exports = {createFile, addInList, getList, deleteFile, getRulesList, addRule, editRule}
+
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const qrcode = require('qrcode-terminal');
 const jimp = require('jimp')
 const axios = require('axios')
 
+const {createFile, addInList, getList, deleteFile, addRule, getRulesList, editRule} = require('./clientRepository');
+
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: { headless: true, args: ['--no-sandbox'] }
@@ -10,25 +12,170 @@ const client = new Client({
 
 client.on('qr', (qr) => {
     qrcode.generate(qr, {small: true});
-    console.log('read qrcode');
 });
 
 client.on('ready', () => {
     console.log('Client is ready!');
 });
 
-client.on('message_create', msg => {
+client.on('message_create', async  msg => {
     const command = msg.body.split(' ')[0];
 
     console.log(command)
     // Cola seu número onde tem o 84848484, sem o 9
-    const sender = msg.from.includes("8796046972") ? msg.to : msg.from
+    const sender = msg.from.includes("7488043170") ? msg.to : msg.from
+
+    if (command === "@brunawn")  msg.react("😉")
+    if(['@everyone','@todes','@here','@channel'].includes(command) ) everyOne(msg)
+
     if (command === "!sticker")  generateSticker(msg, sender)
-    if(['@everyone','@todes','@here','@channel'].includes(msg.body) ) everyOne(msg)
+    if (command === "!balinha") {
+        const chat = await msg.getChat();
+        let gleisin = await client.getContactById("558774006609@c.us")
+        await chat.sendMessage("Aí é com o famoso @"+gleisin.id.user+" 😉",{mentions: [gleisin]})
+    }
+/////// Manipuladores de listas de menções////////
+
+    if(command === "!create") createList(msg);
+    if(command === "!add") {
+        let fileName = msg.body.split(" ")[1]
+        addUserInList(fileName,msg)
+    };
+    if(command === "!delete") {
+
+        deleteList(msg)
+    };
+    if(command === "@go") {
+        let fileName = msg.body.split(" ")[1]
+        userMentions(fileName,msg)
+    };
+
+////////////////////////////////////////////////
+/////////adicionar comandos para o grupo
+if(command === "!rules") {
+    getRules(msg);
+}
+if(command === "!rules:add") {
+    addRuleInList(msg);
+}
+if(command === "!rules:edit") {
+    editRuleInList(msg);
+}
+
+
 
 });
 
 client.initialize();
+
+const getRules = async (msg)=>{
+    try {
+        const sender = msg.from.includes("7488043170") ? msg.to : msg.from
+        let rules = await getRulesList(sender.split("@")[0])
+        if(rules.length <= 0){
+            client.sendMessage(sender,"Lista de regras Vazia!")
+            return;
+        }
+        let newRules = rules.map((x, i) => (i+1)+ " - " + x + "\n").toString()
+        const regex = /\n,/gi;
+        client.sendMessage(sender,"Regras do Grupo: \n"+newRules.replace(regex,"\n"))
+    } catch (error) {
+        msg.reply("❌ Erro ao recuperar regras do grupo")
+    }
+
+}
+
+const addRuleInList = async (msg)=>{
+    try {
+
+        const sender = msg.from.includes("7488043170") ? msg.to : msg.from;
+        let rule = msg.body.substring(msg.body.indexOf(" ")).trim()
+        addRule(sender.split("@")[0],rule)
+
+        client.sendMessage(sender,`Regra atualizada com sucesso!`)
+    } catch (error) {
+        msg.reply("❌ Erro ao atualizada Regra na lista")
+    }
+}
+
+const editRuleInList = async (msg)=>{
+    try {
+
+        const sender = msg.from.includes("7488043170") ? msg.to : msg.from;
+        let rule = msg.body.substring(msg.body.indexOf(" ")).trim()
+        editRule(sender.split("@")[0],rule)
+
+        client.sendMessage(sender,`Regra adicionada com sucesso!`)
+    } catch (error) {
+        msg.reply("❌ Erro ao adicionar Regra na lista")
+    }
+}
+
+
+const createList = async (msg)=>{
+    try {
+        const sender = msg.from.includes("7488043170") ? msg.to : msg.from
+        let argument = "";
+        if(msg.body.includes("-a")) argument = msg.body.substring(msg.body.indexOf("-a")+2).trim();
+
+    createFile(msg.body.split(" ")[1], argument)
+    client.sendMessage(sender,"Lista Criada com Sucesso!")
+    } catch (error) {
+        msg.reply("❌ Erro ao criar lista")
+    }
+
+}
+
+const deleteList = async (msg)=>{
+    try {
+        const sender = msg.from.includes("7488043170") ? msg.to : msg.from
+    deleteFile(msg.body.split(" ")[1])
+    client.sendMessage(sender,"Lista deletada com Sucesso!")
+    } catch (error) {
+        msg.reply("❌ Erro ao deletar lista")
+    }
+
+}
+
+const userMentions = async (filename,msg)=>{
+
+   try {
+    const list = await getList(filename)
+    const chat = await msg.getChat();
+
+    const avaliableUsers = list.users.filter(x => chat.participants.some(d => d.id.user == x.id));
+
+
+        let text = "";
+        if(list.title.length > 0){
+            text += list.title;
+        }
+        let mentions = [];
+
+        for(let user of avaliableUsers) {
+            const contact = await client.getContactById(user.serialized);
+
+            mentions.push(contact);
+            text += `@${contact.id.user} `;
+        }
+
+        await chat.sendMessage(text+"\n", { mentions });
+   } catch (error) {
+        msg.reply("❌ Não foi possivel recuperar a lista")
+   }
+}
+
+const addUserInList = async (filename,msg)=>{
+    try {
+        const users = await msg.getMentions();
+        const sender = msg.from.includes("7488043170") ? msg.to : msg.from
+        addInList(filename, users)
+
+        client.sendMessage(sender,`Dados adicionados com sucesso!`)
+    } catch (error) {
+        msg.reply("❌ Erro ao adicionar na lista")
+    }
+}
 
 const everyOne = async(msg)=> {
     const chat = await msg.getChat();
